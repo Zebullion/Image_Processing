69a051bede7c696826c67d5355e08d7e8dc9f877
Zyto_Rex <jacob.wolford@gmail.com>
2021-07-27 20:31:01 +0000

    Image composite, filter, conversion

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ def main():
   im = Image.open('lotus.png')
   im2 = Image.open('image2.png')
   im1 = Image.open('image1.png')
+  im1 = im1.convert('RGBA')
 
   #pngToJpg(im2,'flower')
  
-  imageFun(im)
+  #imageFun(im)
+
+  mergeThee(im1,im2)
+
+
+
+def mergeThee(image1,image2):
+  """composite  2nd image on top of 1st image. Image 1 and 2 must be same size"""
+  mergedImage = Image.alpha_composite(image1,image2)
+  mergedImage.save('composite.png')
 
 
 def imageFun(image):
   sImage = image.filter(ImageFilter.SHARPEN)
-  sImage = image.rotate(90)
+  
+  
   sImage.save('fun.png')
 
 def pngToJpg(image,fileName):
